215e7ce76cf1ac61e928b95274e7b1ae42a29329
Ubuntu <ubuntu@ip-172-31-42-63.eu-north-1.compute.internal>
2026-07-22 13:20:36 +0000

    Configure AWS EC2 deployment

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:5173"],
+    allow_origins=[ "http://localhost:5173",
+                    "http://16.171.24.67:5173",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
